16538b3faa5107d931678fe6189772822efec7d4
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-19 18:57:52 +0800

    - update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+import schemdraw
+import schemdraw.elements as elm
+with schemdraw.Drawing(file='schematic.svg') as d:
+    elm.Resistor().label('100KΩ')
+    elm.Capacitor().down().label('0.1μF', loc='bottom')
+    elm.Line().left()
+    elm.Ground()
+    elm.SourceV().up().label('10V')
