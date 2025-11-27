46fa8b62e238eaf7f1c68754237abed5f57234b7
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-28 02:13:04 +0800

    - update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import schemdraw
 import schemdraw.elements as elm
-with schemdraw.Drawing(file='schematic.svg') as d:
+with schemdraw.Drawing(file='images/schematic.svg') as d:
     elm.Resistor().label('100KΩ')
     elm.Capacitor().down().label('0.1μF', loc='bottom')
     elm.Line().left()
